7b1203f44fec58f0fbaa82f222548c175ac79a25
tanisha dua <tanisha.dua95@gmail.com>
2024-11-22 16:43:24 +0530

    new code

--- src/classComponent.jsx
+++ src/classComponent.jsx
@@ -20,7 +20,7 @@ class Component extends React.Component{
         return(
             <div>
                 <h1>Counter App</h1>
-                <p>Count:{this.state.count} </p>
+                <p>Count: {this.state.count} </p>
                 <button onClick={this.increaseCount}>Increment</button>
                 <button onClick={this.decreaseCount}>Decrement</button>
             </div>
